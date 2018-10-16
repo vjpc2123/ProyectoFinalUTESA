a55a0b3b85d9c06f1b0c37607abc45a341b4ee58
Víctor José Peña Corcino <vpena@infotep.gov.do>
2018-10-16 16:03:38 -0400

    Inicio Formulario Categoria

---
+++ ProyectoGradoUTESA/CapaPresentacion/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tcVehiculo = new System.Windows.Forms.TabControl();
+            this.tpLista = new System.Windows.Forms.TabPage();
+            this.dtgvVehiculo = new System.Windows.Forms.DataGridView();
+            this.tpDetalle = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnBorrarFilas = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.panelHeader.SuspendLayout();
+            this.tcVehiculo.SuspendLayout();
+            this.tpLista.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvVehiculo)).BeginInit();
+            this.tpDetalle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Controls.Add(this.label1);
+            this.panelHeader.Controls.Add(this.btnCerrar);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(748, 30);
+            this.panelHeader.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Categorias.";
+            // 
+            // tcVehiculo
+            // 
+            this.tcVehiculo.Controls.Add(this.tpLista);
+            this.tcVehiculo.Controls.Add(this.tpDetalle);
+            this.tcVehiculo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcVehiculo.Location = new System.Drawing.Point(0, 30);
+            this.tcVehiculo.Name = "tcVehiculo";
+            this.tcVehiculo.SelectedIndex = 0;
+            this.tcVehiculo.Size = new System.Drawing.Size(748, 427);
+            this.tcVehiculo.TabIndex = 2;
+            // 
+            // tpLista
+            // 
+            this.tpLista.Controls.Add(this.btnEditar);
+            this.tpLista.Controls.Add(this.btnNew);
+            this.tpLista.Controls.Add(this.btnBorrarFilas);
+            this.tpLista.Controls.Add(this.dtgvVehiculo);
+            this.tpLista.Location = new System.Drawing.Point(4, 22);
+            this.tpLista.Name = "tpLista";
+            this.tpLista.Padding = new System.Windows.Forms.Padding(3);
+            this.tpLista.Size = new System.Drawing.Size(740, 401);
+            this.tpLista.TabIndex = 0;
+            this.tpLista.Text = "Lista";
+            this.tpLista.UseVisualStyleBackColor = true;
+            // 
+            // dtgvVehiculo
+            // 
+            this.dtgvVehiculo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgvVehiculo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvVehiculo.Location = new System.Drawing.Point(-4, 40);
+            this.dtgvVehiculo.Name = "dtgvVehiculo";
+            this.dtgvVehiculo.Size = new System.Drawing.Size(748, 316);
+            this.dtgvVehiculo.TabIndex = 14;
+            // 
+            // tpDetalle
+            // 
+            this.tpDetalle.AutoScroll = true;
+            this.tpDetalle.Controls.Add(this.groupBox1);
+            this.tpDetalle.Location = new System.Drawing.Point(4, 22);
+            this.tpDetalle.Name = "tpDetalle";
+            this.tpDetalle.Padding = new System.Windows.Forms.Padding(3);
+            this.tpDetalle.Size = new System.Drawing.Size(740, 401);
+            this.tpDetalle.TabIndex = 1;
+            this.tpDetalle.Text = "Detalle";
+            this.tpDetalle.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(3, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(734, 387);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnEditar.FlatAppearance.BorderSize = 0;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Image = global::CapaPresentacion.Properties.Resources.Edit_Property_25px;
+            this.btnEditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEditar.Location = new System.Drawing.Point(632, 3);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(108, 34);
+            this.btnEditar.TabIndex = 17;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(194)))), ((int)(((byte)(77)))));
+            this.btnNew.FlatAppearance.BorderSize = 0;
+            this.btnNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.ForeColor = System.Drawing.Color.White;
+            this.btnNew.Image = global::CapaPresentacion.Properties.Resources.Add_Property_25px;
+            this.btnNew.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNew.Location = new System.Drawing.Point(511, 3);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(108, 34);
+            this.btnNew.TabIndex = 16;
+            this.btnNew.Text = "Nuevo";
+            this.btnNew.UseVisualStyleBackColor = false;
+            // 
+            // btnBorrarFilas
+            // 
+            this.btnBorrarFilas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBorrarFilas.BackColor = System.Drawing.Color.Red;
+            this.btnBorrarFilas.FlatAppearance.BorderSize = 0;
+            this.btnBorrarFilas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrarFilas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrarFilas.ForeColor = System.Drawing.Color.White;
+            this.btnBorrarFilas.Image = global::CapaPresentacion.Properties.Resources.Trash_25px;
+            this.btnBorrarFilas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBorrarFilas.Location = new System.Drawing.Point(0, 358);
+            this.btnBorrarFilas.Name = "btnBorrarFilas";
+            this.btnBorrarFilas.Size = new System.Drawing.Size(256, 43);
+            this.btnBorrarFilas.TabIndex = 15;
+            this.btnBorrarFilas.Text = "Borrar Registro Seleccionado";
+            this.btnBorrarFilas.UseVisualStyleBackColor = false;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Image = global::CapaPresentacion.Properties.Resources.Close_Window_25px1;
+            this.btnCerrar.Location = new System.Drawing.Point(720, 0);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(28, 30);
+            this.btnCerrar.TabIndex = 12;
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // frmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 457);
+            this.Controls.Add(this.tcVehiculo);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCategoria";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.tcVehiculo.ResumeLayout(false);
+            this.tpLista.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvVehiculo)).EndInit();
+            this.tpDetalle.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.TabControl tcVehiculo;
+        private System.Windows.Forms.TabPage tpLista;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnBorrarFilas;
+        private System.Windows.Forms.DataGridView dtgvVehiculo;
+        private System.Windows.Forms.TabPage tpDetalle;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
--- ProyectoGradoUTESA/CapaPresentacion/Properties/AssemblyInfo.cs
+++ ProyectoGradoUTESA/CapaPresentacion/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CapaPresentacion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
 [assembly: AssemblyProduct("CapaPresentacion")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f88b6a77-f606-435c-aa63-25372edf2169")]
+[assembly: Guid("860f6477-c7df-4ce8-abba-952ec90f9abc")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
